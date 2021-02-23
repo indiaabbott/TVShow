3727ebf26004243b3c5a13483832e19916035756
India Abbott <indiaabbott@hotmail.com>
2021-02-23 15:02:41 +0000

    episode data downloaded

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,24 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  interface IEpisode {
+    id: number;
+    url: string;
+    name: string;
+    season: number;
+    number: number;
+    type: string;
+    airdate: string;
+    airtime: string;
+    airstamp: string;
+    runtime: number;
+    image: {
+      medium: string;
+      original: string;
+    };
+    summary: string;
+    _links: { self: { href: string } };
+  }
   return (
     <div className="App">
       <p>Hello World</p>
